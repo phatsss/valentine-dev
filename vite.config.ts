1a4361ff61681df68b885872553d1b11d60097d1
phatsss <poolphong2@gmail.com>
2025-02-13 14:14:10 +0700

    🔧 chore: Update homepage and base configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "valentine-dev.github.io",
+  base: "valentine-dev",
 });
